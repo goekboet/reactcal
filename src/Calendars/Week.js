7ef1fa55aa9759feed113cd816c95db4cadff707
Erik Göök <erikgook@gmail.com>
2017-04-01 23:27:06 +0200

    - Title on week - Hours in day

--- src/Calendars/Week.js
+++ src/Calendars/Week.js
@@ -1,15 +1,24 @@
 import D from 'date-fns';
 
 const Days = (first, last) => D.eachDay(first, last).map(day => {
-    return { key: D.format(day, 'YYYY-MM-DD'), hours: [] };
+    return { key: D.format(day, 'YYYY-MM-DD'), hours: Consecutive(24) };
 });
+
+const Consecutive = (x) => [...Array(x).keys()].map(i => i.toString())
+
+const Title = (date) =>
+{
+    return "vecka " + D.format(date, 'W');
+}
 
 const Week = (date) => 
 {
     const first = D.startOfISOWeek(date);
     const last = D.addDays(first, 6);
 
-    return { days: Days(first, last)};
+    return { 
+        title: Title(date), 
+        days: Days(first, last)};
 };
 
 export default Week;
--- src/Calendars/Week.test.js
+++ src/Calendars/Week.test.js
@@ -2,9 +2,13 @@ import Week from './Week'
 import testdata from './Week.testdata'
 
 describe('Week calendar', () => {
-    const refdate = new Date(2017, 2, 5)
+    const refdate = new Date(2017, 2, 5);
+    const week = Week(refdate);
+
     it('gives correct days', () => {
-        const days = Week(refdate).days;
-        expect(days).toEqual(testdata.Week.days)
+        expect(week.days).toEqual(testdata.Week.days)
+    });
+    it('gives right title', () => {
+        expect(week.title).toEqual(testdata.Week.title)
     })
 })
