0434f3564347580f1a81f7c18acb67b35c040230
Erik Göök <erikgook@gmail.com>
2017-04-04 22:21:41 +0200

    - Types to Week and basic test of render.

--- src/Week.js
+++ src/Week.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Link from './Link';
 
 const Week = (props) => {
-    return(<Link text="Hello" action={props.monthView} />)
+    return (<Link text="Hello" action={props.monthView} />)
 }
 
 Week.propTypes = {
-    monthView: React.PropTypes.func
+    label: React.PropTypes.string.isRequired,
+    month: React.PropTypes.string.isRequired,
+    days: React.PropTypes.arrayOf(React.PropTypes.shape(
+        {
+            key: React.PropTypes.string.isRequired,
+            hours: React.PropTypes.arrayOf(React.PropTypes.string.isRequired).isRequired
+        })),
+    monthView: React.PropTypes.func.isRequired
 }
 
 export default Week;
---
+++ src/Week.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Week from './Week';
+import testdata from './Calendars/Week.testdata';
+
+describe('Week view', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Week 
+        label={testdata.Week.title}
+        month={testdata.Week.month}
+        days={testdata.Week.days}
+        monthView={() => undefined }/>, div);
+  });
+});
